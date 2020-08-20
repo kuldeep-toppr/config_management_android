7df62b538b94916efd43031f524c560a2ffa50da
kuldeep-toppr <kuldeep.s@toppr.com>
2020-08-20 15:19:38 +0530

    base app

---
+++ Config_management/app/src/main/java/com/example/config_management/activities/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.config_management.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.config_management.R
+
+class SecondActivity: AppCompatActivity()
+{
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
