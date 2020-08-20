3ba30c80fec379bb7389768f05d642e21364140f
kuldeep-toppr <kuldeep.s@toppr.com>
2020-08-20 19:31:48 +0530

    tabLayout and viewPager

--- config_management/app/src/main/java/com/example/config_management/MainActivity.kt
+++ config_management/app/src/main/java/com/example/config_management/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.config_management
+import com.example.config_management.PagerAdapter
+import com.example.config_management.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val pagerAdapter by lazy {
+        PagerAdapter(supportFragmentManager)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pagerAdapter.title.add("Domains")
+        pagerAdapter.title.add("Features")
+        viewpager.adapter = pagerAdapter
+        tabs.setupWithViewPager(viewpager)
+
     }
 }
